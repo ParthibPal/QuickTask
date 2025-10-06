0a2648c02ca93b7d17e9dff6a41766dff7051586
Parthib <palparthib97@gmail.com>
2025-10-06 12:21:48 +0530

    Environment Var setup

--- quicktask-backend/src/main/java/com/quicktask/backend/QuicktaskBackendApplication.java
+++ quicktask-backend/src/main/java/com/quicktask/backend/QuicktaskBackendApplication.java
@@ -1,13 +1,20 @@
 package com.quicktask.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuicktaskBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuicktaskBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Load .env variables
+        Dotenv dotenv = Dotenv.load();
 
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
+        SpringApplication.run(QuicktaskBackendApplication.class, args);
+    }
 }
